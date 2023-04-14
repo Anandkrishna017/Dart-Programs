b74acc3a5ac1f68d2d39786d4ff73016a2dcb8c8
Anandkrishna017 <anandkrishna017@gmail.com>
2023-04-14 11:24:29 +0530

    basics

---
+++ basics.dart
@@ -0,0 +1,16 @@
+void main() {
+  /*print("Enter 2 no.:");
+  var num1=stdin.readLineSync();  // read variable from console
+  var num2=stdin.readLineSync();
+  
+  var number1=int.parse(num1!);
+  var number2=int.parse(num2!);
+  print("sum = ${number1+number2}");*/  
+
+  int a = 10;
+  double b = 20;
+  num num1 = 100.9;   // it is either int or double
+  print(a);
+  print(b);
+  print(num1);
+}
